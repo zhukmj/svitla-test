7322eca96d8410f6aab6d0e912390180fbfdf476
Michael Zhuk <zhukmichael@gmail.com>
2019-10-08 19:44:20 +0300

    Updates

---
+++ precache-manifest.f8a2802f2ec4a4cc2fe2793ae2b2a54f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9da026d1d89fb379a4220c7ff4f382",
+    "url": "/svitla-test/index.html"
+  },
+  {
+    "revision": "9a1d94e2b85063e1fd1d",
+    "url": "/svitla-test/static/js/2.7c547df0.chunk.js"
+  },
+  {
+    "revision": "b92aa9a89d7fcd92b33a",
+    "url": "/svitla-test/static/js/main.df51f874.chunk.js"
+  },
+  {
+    "revision": "b76e013172094324e2be",
+    "url": "/svitla-test/static/js/runtime-main.0dea6347.js"
+  }
+]);
